c4c6f862d5a578d25b219f1c4e5cdfce15311f3c
Aitemir Kuandyk <mooniron.k@gmail.com>
2021-03-18 13:57:28 +0600

    encapsulated run app functionality from main into pkg/app
    Signed-off-by: Aitemir Kuandyk <mooniron.k@gmail.com>

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,53 +1,9 @@
 package main
 
 import (
-	"github.com/UBtrNvME/go-todo-app/pkg/handler"
-	"github.com/UBtrNvME/go-todo-app/pkg/repository"
-	"github.com/UBtrNvME/go-todo-app/pkg/server"
-	"github.com/UBtrNvME/go-todo-app/pkg/service"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-	"github.com/spf13/viper"
-	"log"
-	"os"
+	"github.com/UBtrNvME/go-todo-app/pkg/app"
 )
 
 func main() {
-	// Initializing app's configuration
-	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
-	}
-
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables: %s", err.Error())
-	}
-
-	db, err := repository.NewPostgresDB(repository.Config{
-		Host: viper.GetString("db.host"),
-		Port: viper.GetString("db.port"),
-		Username: viper.GetString("db.username"),
-		DBName: viper.GetString("db.dbname"),
-		SSLMode: viper.GetString("db.sslmode"),
-		Password: os.Getenv("DB_PASSWORD"),
-	})
-	if err != nil {
-		log.Fatalf("failed to initialize db: %s", err.Error())
-	}
-
-	// Constructing dependencies
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
-
-	// Serving server
-	srv := new(server.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running server: %s", err.Error())
-	}
-}
-
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
+	app.Run()
 }
